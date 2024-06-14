41bd0374ab8b70f841c75e22425f71380fe2e423
Gavin <gavin2026@gmail.com>
2024-06-14 15:12:33 +0800

    upgrade

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,43 @@
-const { createVuePlugin } = require('vite-plugin-vue2');
+'use strict'
+// import { defineConfig, loadEnv } from 'vite'
+const { createVuePlugin } = require('vite-plugin-vue2')
+const path = require('path')
+
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
 
 module.exports = {
   plugins: [createVuePlugin()],
-};
+  resolve: {
+    alias: {
+      '@': resolve('src')
+    }
+  },
+  css: {
+    // 预处理器配置项
+    preprocessorOptions: {
+      less: {
+        math: "always",
+      },
+    },
+  },
+  server: {
+    host: '0.0.0.0',
+    proxy: {
+      "^/api/": {
+        target: "http://116.211.148.80:8081/fast-service",
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api\//, ""),
+        cookieDomainRewrite: {
+          '*': '',
+        },
+      },
+    },
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true
+    }
+  },
+}
